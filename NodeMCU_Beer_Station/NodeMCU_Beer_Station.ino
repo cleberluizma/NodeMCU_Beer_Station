78cac3acc9b7b4af2c2d1a05a149f0ffa03eff99
Cleber Miyakoda <cleberluizma@gmail.com>
2022-08-18 23:09:31 -0300

    led da placa "estou on"

--- NodeMCU_Beer_Station/NodeMCU_Beer_Station.ino
+++ NodeMCU_Beer_Station/NodeMCU_Beer_Station.ino
@@ -7,6 +7,8 @@ void setup()
 {
   lcd.init();                      // initialize the lcd   
   lcd.backlight();
+  ///
+ pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   }
 
 void loop()
@@ -15,4 +17,15 @@ void loop()
   lcd.print("    Welcome     ");  
   lcd.setCursor(0,1);
   lcd.print(" MIYAKODA TECH  ");
+  ///
+
+    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  // but actually the LED is on; this is because
+  // it is active low on the ESP-01)
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);
+
+  ///
+  
 }
